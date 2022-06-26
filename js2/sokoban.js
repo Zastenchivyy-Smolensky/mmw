a23cf18d5379470a30cdea51949ea5fbf362c038
Zastenchivyy-Smolensky <s20c2007@bene.fit.ac.jp>
2022-06-27 00:07:33 +0900

    ..

--- js2/sokoban.js
+++ js2/sokoban.js
@@ -1,19 +1,30 @@
 (function () {
   "use strict";
-  const CELL_WIDTH = 11;
-  const CELL_HEIGHT = 11;
-  const WALL_IMG = "";
-  const GOAL_IMG = "";
-  const PLAYER_IMG = "";
-  const BOX_IMG = "";
-  const IN_COLOR = "red";
-  const OUT_COLOR = "red";
-  const GOAL_BIT = 0 * 1;
-  const BOX_BIT = 0 * 2;
-  const PLAYER_BIT = 0 * 4;
+  const CELL_WIDTH = 40;
+  const CELL_HEIGHT = 40;
+  const WALL_IMG = "block.png";
+  const GOAL_IMG = "2.png";
+  const PLAYER_IMG = "index.png";
+  const BOX_IMG = "box.png";
+  const IN_COLOR = "aquamarine";
+  const OUT_COLOR = "whitesmoke";
+
+  const A_UP = 0;
+  const A_RIGHT = 1;
+  const A_DOWN = 2;
+  const A_LEFT = 3;
+  const actions = {
+    ArrowLeft: A_LEFT,
+    ArrowRight: A_RIGHT,
+    ArrowUp: A_UP,
+    ArrowDown: A_DOWN,
+  };
+  const GOAL_BIT = 0x01;
+  const BOX_BIT = 0x02;
+  const PLAYER_BIT = 0x04;
   const WALL_VAL = 8;
   const OUT_VAL = 9;
-  const probelems = [
+  const problems = [
     [
       [9, 9, 9, 8, 8, 8, 8, 8],
       [8, 8, 8, 8, 0, 0, 0, 8],
@@ -23,12 +34,26 @@
       [8, 8, 8, 8, 8, 8, 8, 9],
     ],
     [
-      [9, 9, 9, 8, 8, 8, 8, 8, 9],
-      [8, 8, 8, 8, 0, 0, 0, 8, 9],
-      [8, 1, 0, 2, 0, 1, 0, 8, 8],
-      [8, 0, 8, 2, 8, 0, 8, 8, 9],
-      [8, 0, 0, 4, 0, 0, 8, 9, 8],
-      [8, 8, 8, 8, 8, 8, 8, 9, 9],
+      [9, 8, 8, 8, 8, 8, 8, 8, 9],
+      [9, 8, 0, 4, 1, 1, 1, 8, 9],
+      [9, 8, 0, 0, 0, 8, 8, 8, 8],
+      [8, 8, 8, 2, 0, 0, 0, 0, 8],
+      [8, 0, 0, 0, 8, 2, 8, 0, 8],
+      [8, 0, 2, 0, 8, 0, 0, 0, 8],
+      [8, 0, 2, 0, 8, 0, 0, 0, 8],
+      [8, 0, 0, 0, 8, 8, 8, 8, 8],
+      [8, 8, 8, 8, 8, 9, 9, 9, 9],
+    ],
+    [
+      [8, 8, 8, 8, 8, 8, 8],
+      [8, 0, 0, 8, 0, 0, 8],
+      [8, 0, 2, 0, 2, 0, 8],
+      [8, 0, 1, 8, 1, 0, 8],
+      [8, 0, 0, 1, 2, 4, 8],
+      [8, 0, 1, 8, 1, 0, 8],
+      [8, 0, 2, 0, 2, 0, 8],
+      [8, 0, 0, 8, 0, 0, 8],
+      [8, 8, 8, 8, 8, 8, 8],
     ],
   ];
   let xPlayer;
@@ -53,6 +78,72 @@
   let canvasElem;
   let ctx;
 
+  function onKeyDown(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    doAction(actions[e.code]);
+  }
+  function doAction(action) {
+    if (action == null) return;
+    let xp = xPlayer;
+    let yp = yPlayer;
+    let xb = xp;
+    let yb = yp;
+    switch (action) {
+      case A_LEFT:
+        --xp;
+        xb = xp - 1;
+        break;
+      case A_RIGHT:
+        ++xp;
+        xb = xp + 1;
+        break;
+      case A_UP:
+        --yp;
+        yb = yp - 1;
+        break;
+      case A_DOWN:
+        ++yp;
+        yb = yp + 1;
+        break;
+      default:
+        return;
+    }
+    if (board[yp][xp] >= WALL_VAL) return;
+    if (!(board[yp][xp] & BOX_BIT)) {
+      board[yPlayer][xPlayer] &= ~PLAYER_BIT;
+      drawCell(xPlayer, yPlayer);
+      board[yp][xp] |= PLAYER_BIT;
+      xPlayer = xp;
+      yPlayer = yp;
+      drawPlaer();
+      ++step;
+      stepElem.textContent = step.toString();
+    } else if (board[yb][xb] < WALL_VAL && !(board[yb][xb] & BOX_BIT)) {
+      board[yp][xp] &= ~BOX_BIT;
+      board[yb][xb] |= BOX_BIT;
+      drawCell(xp, yp);
+      drawCell(xb, yb);
+      board[yPlayer][xPlayer] &= ~PLAYER_BIT;
+      drawCell(xPlayer, yPlayer);
+      board[yp][xp] |= PLAYER_BIT;
+      xPlayer = xp;
+      yPlayer = yp;
+      drawPlaer();
+
+      ++step;
+      stepElem.textContent = step.toString();
+      ++push;
+
+      pushElem.textContent = push.toString();
+      if (isGoal()) {
+        stopGame();
+        setTimeout(() => {
+          alert("CLEAR");
+        }, 300);
+      }
+    }
+  }
   function drawPlaer() {
     ctx.drawImage(
       iPlayer,
@@ -87,19 +178,34 @@
         CELL_HEIGHT
       );
     }
-    function drawBoard() {
-      for (let y = 0; y < board.length; ++y) {
-        for (let x = 0; x < board[y].length; ++x) {
-          drawCell(x, y);
-        }
-      }
-      drawPlaer();
+    if (b & BOX_BIT) {
+      ctx.drawImage(
+        iBox,
+        x * CELL_WIDTH,
+        y * CELL_HEIGHT,
+        CELL_WIDTH,
+        CELL_HEIGHT
+      );
     }
-    function getGoalList() {
-      const a = new Array();
-      let cp = 0;
-      let cb = 0;
-      for (let y = 0; y < board.length; ++y) {
+  }
+  function drawBoard() {
+    for (let y = 0; y < board.length; ++y) {
+      for (let x = 0; x < board[y].length; ++x) {
+        drawCell(x, y);
+      }
+    }
+    drawPlaer();
+  }
+
+  function isGoal() {
+    return goals.every((g) => board[g[1][g[0]]] & BOX_BIT);
+  }
+  function getGoalList() {
+    const a = new Array();
+    let cp = 0;
+    let cb = 0;
+    for (let y = 0; y < board.length; ++y) {
+      for (let x = 0; x < board[y].length; ++x) {
         const b = board[y][x];
         if (b >= WALL_VAL) continue;
         if (b & GOAL_BIT) a.push([x, y]);
@@ -111,6 +217,7 @@
         }
       }
     }
+
     if (a.length === 0) {
       alert("ゴールがありません");
     }
@@ -122,16 +229,17 @@
     }
     return a;
   }
+
   function countUp() {
-    etimeElem.textContent = (Date.now() - startTime) / 1000 / toFixed();
+    etimeElem.textContent = ((Date.now() - startTime) / 1000).toFixed();
   }
 
   function startGeme() {
-    const s = Math.floor(Math.random() * probelems.length) + 1;
+    const s = Math.floor(Math.random() * problems.length) + 1;
 
     board = new Array();
-    for (let i = 0; i < probelems[s - 1].length; ++i) {
-      board[i] = probelems[s - 1][i].slice();
+    for (let i = 0; i < problems[s - 1].length; ++i) {
+      board[i] = problems[s - 1][i].slice();
     }
     goals = getGoalList();
     canvasWidth = CELL_WIDTH * board[0].length;
@@ -139,7 +247,9 @@
     canvasElem.width = canvasWidth;
     canvasElem.height = canvasHeight;
     ctx = canvasElem.getContext("2d");
+    ctx.fillStyle = OUT_COLOR;
     drawBoard();
+
     countUpTimer = setInterval(countUp, 400);
 
     step = 0;
@@ -147,11 +257,13 @@
     push = 0;
     pushElem.textContent = push.toString();
     startTime = Date.now();
+    document.body.addEventListener("keydown", onKeyDown, true);
   }
-  function stepGame() {
+  function stopGame() {
     if (!countUpTimer) return;
     clearInterval(countUpTimer);
     countUpTimer = undefined;
+    document.body.removeEventListener("keydown", onkeydown, true);
   }
   class ImageLoader {
     set(...names) {
@@ -160,7 +272,7 @@
         a[i] = new Image();
         a[i].addEventListener("load", this.loaded.bind(this), { once: true });
       }
-      this.loading = name.length;
+      this.loading = names.length;
       for (let i = 0; i < names.length; ++i)
         a[i].src = names[i] + "?ms=" + Date.now();
       return a;
@@ -189,4 +301,4 @@
     );
   }
   document.addEventListener("DOMContentLoaded", init);
-});
+})();
